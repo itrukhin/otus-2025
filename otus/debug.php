5bbad1498a67dfcab3f116079e625ed4e13223cc
itrukhin <itrukhin@gmail.com>
2025-12-13 20:26:34 +0300

    Запись в лог файл

---
+++ otus/debug.php
@@ -0,0 +1,20 @@
+<?php
+use Bitrix\Main\Diag\Debug;
+
+/** @var \CMain $APPLICATION */
+global $APPLICATION;
+
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("Тестирование логов");
+
+$dateTime = new \Bitrix\Main\Type\DateTime();
+dump("Запись в лог " . $dateTime);
+Debug::writeToFile($dateTime);
+
+?>
+<textarea style="width: 100%; height: 500px;">
+<?=file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/__bx_log.log')?>
+</textarea>
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
