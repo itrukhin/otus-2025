96bdb5852aca773a001b8a6d527dcfc5ccf67e15
itrukhin <itrukhin@gmail.com>
2025-12-20 10:10:22 +0300

    Материалы ДЗ-6

---
+++ local/App/Models/Lists/CarCityPropertyValuesTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Lists;
+
+use App\Models\AbstractIblockPropertyValuesTable;
+
+class CarCityPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+    const IBLOCK_ID = 17;
+}
---
+++ local/App/Models/Lists/CarManufacturerPropertyValuesTable.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Lists;
+
+use App\Models\AbstractIblockPropertyValuesTable;
+
+class CarManufacturerPropertyValuesTable extends AbstractIblockPropertyValuesTable
+{
+    const IBLOCK_ID = 18;
+}
