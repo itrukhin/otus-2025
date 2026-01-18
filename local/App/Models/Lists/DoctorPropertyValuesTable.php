47313ec1bcd110ae8ac9a6b5bef8285bd34dbe4f
itrukhin <itrukhin@gmail.com>
2026-01-18 11:30:10 +0300

    ДЗ #4

--- local/App/Models/Lists/DoctorPropertyValuesTable.php
+++ local/App/Models/Lists/DoctorPropertyValuesTable.php
@@ -2,8 +2,28 @@
 namespace App\Models\Lists;
 
 use App\Models\AbstractIblockPropertyValuesTable;
+use Bitrix\Main\Entity\ReferenceField;
+use Bitrix\Main\ORM\Query\Join;
 
+/*
+ * Свойства:
+ * PROCEDURES - список процедур ProcedurePropertyValuesTable
+ * DEPARTMENT_ID - отделение integer
+ */
 class DoctorPropertyValuesTable extends AbstractIblockPropertyValuesTable
 {
     public const IBLOCK_ID = 16;
+
+//    public static function getMap(): array
+//    {
+//        $map = [
+//            'PROCEDURE_LIST' => (new ReferenceField(
+//                'PROCEDURE_LIST',
+//                ProcedurePropertyValuesTable::class,
+//                Join::on('this.PROCEDURES', 'ref.IBLOCK_ELEMENT_ID')
+//            ))->configureJoinType(Join::TYPE_LEFT),
+//        ];
+//
+//        return parent::getMap() + $map;
+//    }
 }
